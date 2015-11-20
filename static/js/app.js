13e50713d129b79b30dddaf60a2a7ed34cb7da4b
Relsi Maron <relsi@codeup.com.br>
2015-11-20 20:22:38 -0200

    Adicona listagem de pontos de localização na página da planta

--- static/js/app.js
+++ static/js/app.js
@@ -7,7 +7,34 @@ $(function () {
         map: "#add_point_map",
         details: "form"
     });
-
-
-
 })
+
+
+//Marker window click
+function bindInfoWindow(marker, map, infoWindow, html) {
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(html);
+    infoWindow.open(map, marker);
+  });
+}
+
+
+//AJAX function to load map points
+//TODO: Change it to jQuery - Sorry puritans :)
+function downloadUrl(url,callback) {
+ var request = window.ActiveXObject ?
+     new ActiveXObject('Microsoft.XMLHTTP') :
+     new XMLHttpRequest;
+
+ request.onreadystatechange = function() {
+   if (request.readyState == 4) {
+     request.onreadystatechange = doNothing;
+     callback(request, request.status);
+   }
+ };
+
+ request.open('GET', url, true);
+ request.send(null);
+}
+
+function doNothing() {}
